76ebb9385ec760661e45c25b134934121310c99d
sreejithkaippenchery <117289520+sreejithkaippenchery@users.noreply.github.com>
2023-02-11 00:49:26 +0530

    project upload

---
+++ getdata.py
@@ -0,0 +1,28 @@
+from getexif import *
+from scanfiles import scanfiles
+import cv2
+def getdata():
+    path=input("[+] Enter path for your dataset : ")
+    file_data=scanfiles(path)
+    gps_data=[]
+    date_data=[]
+    if len(file_data):
+        for i in range(0,len(file_data)):
+            exif,filename=get_exif(path+'/'+file_data[i])
+
+            gpstags = get_gpstags(exif,filename)
+
+            date=get_datetags(exif,filename)
+
+            if gpstags:
+                gps_cord=get_coordinates(gpstags)
+            else:
+                gps_cord=None,None
+
+            gps_data.append(gps_cord)
+            date_data.append(date)
+        print("Data processed from the given datasets")
+        return gps_data,date_data
+
+
+        
